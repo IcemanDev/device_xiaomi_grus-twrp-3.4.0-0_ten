94966c5562212f2c67e3f9184eb850f04132048c
IceMan <cammaratamichele1966@gmail.com>
2020-12-29 14:35:12 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,19 +9,17 @@ if [[ -d out/target/product/grus/recovery/root/twres ]]; then
 	rm -rf "$workdir"/out/target/product/grus/recovery/root/sbin/iceroot ; 
 fi
 cp -rf "$workdir"/bootable/recovery/gui/theme/common/portrait.xml "$workdir"/device/xiaomi/grus/file-root/portrait.xml
-sed -i 's/<page name="advanced">/ \
-		<page name="advanced"> \
-			<button style="tab"> \
-				<placement x="%center_x%" y="%tab_y%" placement="4"\/> \
-				<image resource="space"\/> \
-				<text>ROOT \/ UNROOT<\/text> \
+
+sed -i 's/<listitem name="{@reboot_install_app_hdr=Install TWRP App}">/ \
+			<listitem name="ROOT \/ UNROOT">  \
 				<actions> \
 					<action function="queueclear"\/> \
 					<action function="set">tw_selectimage=0<\/action> \
 					<action function="page">flash_confirm_ICEMAN<\/action> \
 					<action function="set">tw_zip_location=\/sbin\/iceroot<\/action> \
 				<\/actions> \
-			<\/button> \
+			<\/listitem> \
+			<listitem name="{@reboot_install_app_hdr=Install TWRP App}"> \
 /g' "$workdir"/device/xiaomi/grus/file-root/portrait.xml
 sed -i '/^	<\/pages>$/,/^<\/recovery>$/d' "$workdir"/device/xiaomi/grus/file-root/portrait.xml
 cat "$workdir"/device/xiaomi/grus/file-root/patch_portrait_normal >> "$workdir"/device/xiaomi/grus/file-root/portrait.xml
